245a9bfdd9119cf3fb0f682101fc1a75362c426a
arunstop <arunstop@gmail.com>
2021-01-28 09:35:21 +0700

    installed video.js

---
+++ src/components/miscs/MainVideoPlayer.vue
@@ -0,0 +1,44 @@
+<template>
+  <video class="video-js vjs-theme-sea vjs-big-play-centered" :ref="elementRef"/>
+</template>
+
+<script>
+import videojs from "video.js";
+import "video.js/dist/video-js.min.css";
+// Forest
+
+export default {
+  props: {
+    elementRef : String,
+    options: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  data() {
+    return {
+      player: null,
+    };
+  },
+  mounted() {
+      this.player = videojs(
+      this.$refs[this.elementRef],
+      this.options,
+      function onPlayerReady() {
+        // console.log("onPlayerReady", this);
+      }
+    );
+  },
+  beforeDestroy() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  },
+};
+</script>
+<style scoped>
+@import 'https://unpkg.com/@videojs/themes@1/dist/sea/index.css';
+
+</style>
--- src/components/pages/game/parts/GameMedia.vue
+++ src/components/pages/game/parts/GameMedia.vue
@@ -3,9 +3,12 @@
     class="rounded-xl"
     hide-delimiter-background
     interval="4800"
+    progress
+    hide-delimiters
     show-arrows-on-hover
+    height="500p"
   >
-    <v-carousel-item v-if="gameDetails.data.clip">
+    <!-- <v-carousel-item v-if="gameDetails.data.clip">
       <v-hover v-slot="{ hover }">
         <v-sheet height="100%">
           <video
@@ -18,17 +21,35 @@
           <v-fade-transition>
             <v-overlay v-if="hover" absolute opacity="0.6">
               <v-row no-gutters align="center">
-                <v-btn class="pa-6 mx-2" icon @click="muteVideo" width="30" height="30">
+                <v-btn
+                  class="pa-6 mx-2"
+                  icon
+                  @click="vid.muteVideo"
+                  width="30"
+                  height="30"
+                >
                   <v-icon size="30">
                     {{ videoPreview.muteIcon }}
                   </v-icon>
                 </v-btn>
-                <v-btn class="pa-4 mx-2" icon @click="toggleVideo" width="60" height="60">
+                <v-btn
+                  class="pa-4 mx-2"
+                  icon
+                  @click="vid.toggleVideo"
+                  width="60"
+                  height="60"
+                >
                   <v-icon size="60">
                     {{ videoPreview.statusIcon }}
                   </v-icon>
                 </v-btn>
-                <v-btn class="pa-6 mx-2" icon @click="screenVideo" width="30" height="30">
+                <v-btn
+                  class="pa-6 mx-2"
+                  icon
+                  @click="vid.screenVideo"
+                  width="30"
+                  height="30"
+                >
                   <v-icon size="30">
                     {{ videoPreview.screenIcon }}
                   </v-icon>
@@ -38,6 +59,9 @@
           </v-fade-transition>
         </v-sheet>
       </v-hover>
+    </v-carousel-item> -->
+    <v-carousel-item>
+      <main-video-player :options="videoOptions" :elementRef="'videoPreview'" />
     </v-carousel-item>
     <v-carousel-item>
       <v-img
@@ -52,9 +76,60 @@
 
 <script>
 import { mapState } from "vuex";
+import MainVideoPlayer from "../../../miscs/MainVideoPlayer.vue";
 export default {
+  components: { MainVideoPlayer },
   computed: {
     ...mapState(["gameDetails"]),
+    vid() {
+      let video = this.$refs["vid-preview"];
+      let vidPrev = this.videoPreview;
+      return {
+        toggleVideo() {
+          video.onplaying = () => {
+            vidPrev.statusIcon = "mdi-pause";
+          };
+          video.onpause = () => {
+            vidPrev.statusIcon = "mdi-play";
+          };
+
+          if (video.paused) {
+            video.play();
+          } else {
+            video.pause();
+            // this.videoPreview.statusIcon = "mdi-play";
+          }
+        },
+        screenVideo() {
+          if (video.mozRequestFullScreen) {
+            video.mozRequestFullScreen();
+          } else if (video.webkitRequestFullScreen) {
+            video.webkitRequestFullScreen();
+          }
+        },
+        muteVideo() {
+          video.muted = !video.muted;
+          if (vidPrev.muteIcon === "mdi-volume-high") {
+            vidPrev.muteIcon = "mdi-volume-mute";
+          } else {
+            vidPrev.muteIcon = "mdi-volume-high";
+          }
+        },
+      };
+    },
+    videoOptions() {
+      return {
+        autoplay: true,
+        controls: true,
+        fluid: true,
+        sources: [
+          {
+            src: this.gameDetails.data.clip.clips["640"],
+            type: "video/mp4",
+          },
+        ],
+      };
+    },
   },
   data: function () {
     return {
@@ -63,47 +138,9 @@ export default {
         screenIcon: "mdi-fullscreen",
         muteIcon: "mdi-volume-high",
       },
-      hoverLayout: false
     };
   },
-  methods: {
-    toggleVideo() {
-      let video = this.$refs["vid-preview"];
-
-      video.onplaying = () => {
-        this.videoPreview.statusIcon = "mdi-pause";
-      };
-      video.onpause = () => {
-        this.videoPreview.statusIcon = "mdi-play";
-      };
-
-      if (video.paused) {
-        video.play();
-      } else {
-        video.pause();
-        // this.videoPreview.statusIcon = "mdi-play";
-      }
-    },
-    screenVideo() {
-      let video = this.$refs["vid-preview"];
-
-      if (video.mozRequestFullScreen) {
-        video.mozRequestFullScreen();
-      } else if (video.webkitRequestFullScreen) {
-        video.webkitRequestFullScreen();
-      }
-    },
-    muteVideo() {
-      let video = this.$refs["vid-preview"];
-
-      video.muted = !video.muted
-      if (this.videoPreview.muteIcon === "mdi-volume-high") {
-        this.videoPreview.muteIcon = "mdi-volume-mute";
-      } else {
-        this.videoPreview.muteIcon = "mdi-volume-high";
-      }
-    },
-  },
+  methods: {},
 };
 </script>
 
