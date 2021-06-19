e71cf3c5eb92e2417b3715d3c5213a9df0f3a0e4
arunstop <arunstop@gmail.com>
2021-06-19 18:54:23 +0700

    REFRESHED API KEY

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -10,9 +10,9 @@ export default {
         let baseUrlSet = {
             rawg: (module) => {
                 let apiUrl = "https://api.rawg.io/api/"
-                // let key = "8cce4b01b3ba424f8d0a405393a02f25"
-                // return apiUrl + module + '?key=' + key
-                return apiUrl + module
+                let key = "f2bdceb43c6e4ac88f5e984bf1ab6430"
+                return apiUrl + module + '?key=' + key
+                // return apiUrl + module
             },
             github: {
                 countryList: 'https://gist.githubusercontent.com/DmytroLisitsyn/1c31186e5b66f1d6c52da6b5c70b12ad/raw/01b1af9b267471818f4f8367852bd4a2814cbae6/country_dial_info.json'
